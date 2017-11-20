b6b793c100fe13ce72f8e4f66793d9e40faefd57
Ashlee Stephenson <ashleeste@gmail.com>
2017-11-20 18:21:04 +0100

    initialized controllers

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,7 +1,16 @@
 class DestinationsController < ApplicationController
   def index
+    search_item = params[:search_item]
+    raise
+    #if user.input == menu_items.type && <= menu_items.price
+     # @destinations.find(menu_item)
+    #else
+     # Destination.all
+    #end
+    @destinations = Destination.all
   end
 
   def show
+    @destinations = Destination.find(params[:id])
   end
 end
