3618e04b0acb60ad9c435ff75c948cdb99077b31
Ashlee Stephenson <ashleeste@gmail.com>
2017-11-20 15:49:37 +0100

    routes completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get "map" to: "destinations#map"
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :destinations do
+    resources :menus, only: [:show]
+  end
 end
