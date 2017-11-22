214a55f621997a948c513d34c2bd05063be9ea85
Ashlee Stephenson <ashleeste@gmail.com>
2017-11-22 18:21:18 +0100

    added search logic to model to filter menu_items by category and price

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,19 @@
 class PagesController < ApplicationController
 
   def home
+  	 # SELECT ALL 
+  	# FROM DESTINATIONS TABLE
+  	# WHERE DESTINATION.LATITUDE && DESTINATION.LONGITUDE (WITHIN 10KM?) MATCHES USER.LATITUDE && USER.LONGITUDE
+  	# categories = ??? 
+  	# price = ???
+  	# @destinations = Destination.near(location, 3)
+  	# @destinations_and_menu_items = @destinations.map do |destination|
+  	#   { destination => destination.filtered_menu_items(categories, price)}
+  	# end
+  	# display destination card w/ cate
   end
 
   def search
-
   end
 
 end
@@ -15,3 +24,5 @@ end
 # based on lat & long of user
 
 
+# SELECT ALL DESTINATIONS WHERE THE LOCATION MATCHES USER LOCATION, THE PRICE IS <= PRICE ENTERED, 
+# AND THE CATEGORY MATCHES CATEGORY(S) SELECTED
