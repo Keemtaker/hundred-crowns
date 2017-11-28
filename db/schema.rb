1f54c648af810ef4796952e9c80903b6ff79acc1
Hakeem Baba <keemtaker@yahoo.com>
2017-11-28 19:16:53 +0100

    created review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122102043) do
+ActiveRecord::Schema.define(version: 20171128174035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,4 +52,15 @@ ActiveRecord::Schema.define(version: 20171122102043) do
     t.index ["destination_id"], name: "index_menu_items_on_destination_id", using: :btree
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.string   "author"
+    t.integer  "rating"
+    t.integer  "destination_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["destination_id"], name: "index_reviews_on_destination_id", using: :btree
+  end
+
+  add_foreign_key "reviews", "destinations"
 end
